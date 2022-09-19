ba48d3572d271d96a86fc49685e4aa5a5453a7f2
satkur <pathfinder10.vox@gmail.com>
2022-09-19 19:23:16 +0900

    初回コミット

---
+++ main.cpp
@@ -0,0 +1,208 @@
+﻿#include "framework.h"
+#include "resource.h"
+
+#define MAX_LOADSTRING 100
+
+// グローバル変数:
+HINSTANCE hInst;                                // 現在のインターフェイス
+WCHAR szTitle[MAX_LOADSTRING];                  // タイトル バーのテキスト
+WCHAR szWindowClass[MAX_LOADSTRING];            // メイン ウィンドウ クラス名
+
+// EditTextBoxのハンドル
+HWND hEdit;
+HFONT hFont;
+
+// このコード モジュールに含まれる関数の宣言を転送します:
+ATOM             RegisterWindowClass(HINSTANCE hInstance);
+BOOL             InitInstance(HINSTANCE, int);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
+
+void CreateEditTextBox(HWND hWnd);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+					  _In_opt_ HINSTANCE hPrevInstance,
+					  _In_ LPWSTR    lpCmdLine,
+					  _In_ int       nCmdShow) {
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// TODO: ここにコードを挿入してください。
+
+	// グローバル文字列を初期化する
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_SIMPLETASKREMINDER, szWindowClass, MAX_LOADSTRING);
+	RegisterWindowClass(hInstance);
+
+	// アプリケーション初期化の実行:
+	if (!InitInstance(hInstance, nCmdShow)) {
+		return FALSE;
+	}
+
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_SIMPLETASKREMINDER));
+
+	MSG msg;
+
+	// メイン メッセージ ループ:
+	while (GetMessage(&msg, nullptr, 0, 0)) {
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int)msg.wParam;
+}
+
+//
+//  関数: RegisterWindowClass()
+//
+//  目的: ウィンドウ クラスを登録します。
+//
+ATOM RegisterWindowClass(HINSTANCE hInstance) {
+	WNDCLASSEXW wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SIMPLETASKREMINDER));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_SIMPLETASKREMINDER);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassExW(&wcex);
+}
+
+//
+//   関数: InitInstance(HINSTANCE, int)
+//
+//   目的: インスタンス ハンドルを保存して、メイン ウィンドウを作成します
+//
+//   コメント:
+//
+//        この関数で、グローバル変数でインスタンス ハンドルを保存し、
+//        メイン プログラム ウィンドウを作成および表示します。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
+	hInst = hInstance; // グローバル変数にインスタンス ハンドルを格納する
+
+	HWND hWnd = CreateWindowW(
+		szWindowClass,
+		szTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr);
+
+	if (!hWnd) {
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+
+//
+//  関数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目的: メイン ウィンドウのメッセージを処理します。
+//
+//  WM_COMMAND  - アプリケーション メニューの処理
+//  WM_PAINT    - メイン ウィンドウを描画する
+//  WM_DESTROY  - 中止メッセージを表示して戻る
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch (message) {
+		case WM_CREATE:
+			CreateEditTextBox(hWnd);
+			break;
+
+		case WM_COMMAND: {
+				int wmId = LOWORD(wParam);
+				// 選択されたメニューの解析:
+				switch (wmId) {
+					case IDM_ABOUT:
+						DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+						break;
+					case IDM_EXIT:
+						DestroyWindow(hWnd);
+						break;
+					default:
+						return DefWindowProc(hWnd, message, wParam, lParam);
+				}
+			}
+			break;
+		case WM_PAINT: {
+				PAINTSTRUCT ps;
+				HDC hdc = BeginPaint(hWnd, &ps);
+				// TODO: HDC を使用する描画コードをここに追加してください...
+				EndPaint(hWnd, &ps);
+			}
+			break;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// バージョン情報ボックスのメッセージ ハンドラーです。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message) {
+		case WM_INITDIALOG:
+			return (INT_PTR)TRUE;
+
+		case WM_COMMAND:
+			if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
+				EndDialog(hDlg, LOWORD(wParam));
+				return (INT_PTR)TRUE;
+			}
+			break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+// EditTextBoxの生成
+void CreateEditTextBox(HWND hWnd) {
+	// EditTextBoxウィンドを生成
+	hEdit = CreateWindowW(
+		_T("EDIT"), //ウィンドウクラス名
+		NULL, //キャプション
+		WS_CHILD | WS_VISIBLE | WS_BORDER |
+		WS_HSCROLL | WS_VSCROLL | ES_AUTOHSCROLL | ES_AUTOVSCROLL |
+		ES_LEFT | ES_MULTILINE, //スタイル指定
+		10, 10, //位置
+		480, 300, //幅、高さ
+		hWnd, //親ウィンドウ
+		(HMENU)1, // メニューハンドルまたは子ウィンドウID
+		hInst, //インスタンスハンドル
+		NULL); //その他の作成データ
+
+	// フォントの作成
+	hFont = CreateFont(
+		18, 0, 0, 0,
+		FW_NORMAL, FALSE, FALSE, 0,
+		SHIFTJIS_CHARSET,
+		OUT_DEFAULT_PRECIS,
+		CLIP_DEFAULT_PRECIS,
+		DEFAULT_QUALITY, FIXED_PITCH, _T("MS ゴシック"));
+
+	// フォント変更のメッセージを送信
+	SendMessage(hEdit, WM_SETFONT, (WPARAM)hFont, MAKELPARAM(FALSE, 0));
+}
